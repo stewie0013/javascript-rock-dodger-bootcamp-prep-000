83f29d48297f80cf6854e082cc730097b9904ad9
noah.stewart9813@gmail.com <noah.stewart9813@gmail.com>
2018-08-13 20:22:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -119,6 +119,13 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
+   document.addEventListener('keydown', function(e) {
+     if (e.which === RIGHT_ARROW) {
+       moveDodgerRight()
+     } else if (e.which === LEFT_ARROW) {
+       moveDodgerLeft()
+     }
+   })
 }
 
 function moveDodgerLeft() {
@@ -127,6 +134,7 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   
 }
 
 function moveDodgerRight() {
@@ -135,6 +143,7 @@ function moveDodgerRight() {
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   
 }
 
 /**
