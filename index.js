36ea9bf217ec1b1e152e78e4239ca9619e156046
noah.stewart9813@gmail.com <noah.stewart9813@gmail.com>
2018-08-13 20:48:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -138,6 +138,7 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   
 }
 
 function moveDodgerRight() {
